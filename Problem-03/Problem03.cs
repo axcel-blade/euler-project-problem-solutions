e4d1e816a52bde877770f3f535d505c0eab50366
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-09-28 12:05:04 +0530

    Update Problem03.cs

--- Problem-03/Problem03.cs
+++ Problem-03/Problem03.cs
@@ -6,6 +6,18 @@ public class Problem03
 {
     public static void Main(string[] args)
     {
-        
+        long Divisor = 1, Quotient = 0;
+        const long Dividend = 600851475143;
+
+        while(Divisor <= Dividend)
+        {
+            if(Dividend % Divisor == 0)
+            {
+                Quotient = Dividend % Divisor;
+            }
+            Divisor = Divisor + 1;
+        }
+
+        Console.WriteLine("Largest prime factor : {0}", Quotient);
     }
 }
