1d9e414d5780f646a0dbe1e478300d1fdd583245
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-09-29 19:59:53 +0530

    Update Problem04.cs

--- Problem-04/Problem04.cs
+++ Problem-04/Problem04.cs
@@ -6,30 +6,39 @@ public class Problem04
 {
     public static void Main(string[] args)
     {
-        double MultipliedNumber = 0, LargestPalindromeNumber = 0, ReversedNumber = 0;
+        long MultipliedNumber = 0, LargestPalindromeNumber = 0, ReversedNumber = 0;
+        string ReversedNumberString = "";
 
         for(int i = 100; i < 1000; i++)
         {
             for(int j = 100; j < 1000; j++)
             {
                 MultipliedNumber = i * j;
+                string MultipliedNumberString = MultipliedNumber.ToString();
+                //Console.WriteLine(MultipliedNumberString);
+                //Console.WriteLine(MultipliedNumberString.Length);
 
                 //Reverse the multiplied numbers
-                for(int k = 4; k >= 1; k--)
+                for(int k = MultipliedNumberString.Length; k > 1; k--)
                 {
                     //Get the quotient of the multiplied numbers
-                    double Quotient = Math.Floor(MultipliedNumber / Math.Pow(10, k-1));
+                    //double Quotient = Math.Floor(MultipliedNumber / Math.Pow(10, k-1));
 
                     //Get the remainder of the multiplied numbers
-                    double Remainder = MultipliedNumber % Math.Pow(10, k-1);
+                    //double Remainder = MultipliedNumber % Math.Pow(10, k-1);
 
                     //Console.WriteLine(Quotient);
                     //Console.WriteLine(Remainder);
 
-                    ReversedNumber = ReversedNumber + Quotient * Math.Pow(10, k);
+                    //ReversedNumber = ReversedNumber + (Quotient * Math.Pow(10, k-1));
 
-                    MultipliedNumber = Remainder;
+                    //MultipliedNumber = Remainder;
+
+                    ReversedNumberString = ReversedNumberString + MultipliedNumberString[k-1];
+                    Console.WriteLine(ReversedNumberString);
                 }
+
+                ReversedNumber = Int64.Parse(ReversedNumberString);
 
                 if(ReversedNumber == MultipliedNumber)
                 {
