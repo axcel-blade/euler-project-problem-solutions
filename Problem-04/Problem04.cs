07de6afa319c24d763bca64eb3b591452e5eaf78
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-09-30 07:30:45 +0530

    Update Problem04.cs

--- Problem-04/Problem04.cs
+++ Problem-04/Problem04.cs
@@ -6,32 +6,39 @@ public class Problem04
 {
     public static void Main(string[] args)
     {
-        int MultipliedNumber = 0, ReversedNumber = 0, Remainder = 0, Number = 0, LargestPalindromeProduct = 0;
+        int MultipliedNumber = 0, ReversedNumber = 0, Number = 0, Remainder = 0, NumberOne = 10, NumberTwo = 10;
 
-        for(int i = 91; i < 99; i = i + 1)
+        while(NumberOne < 91)
         {
-            for(int j = 91; j < 99; j = j + 1)
+            while(NumberTwo < 99)
             {
-                MultipliedNumber = i * j;
+                MultipliedNumber = NumberOne * NumberTwo;
                 Number = MultipliedNumber;
+                //Console.WriteLine(Number);
 
                 while(Number != 0)
                 {
                     Remainder = Number % 10;
+                    //Console.WriteLine(Remainder);
+
                     ReversedNumber = ReversedNumber * 10 + Remainder;
+                    //Console.WriteLine(ReversedNumber);
+
                     Number = Number / 10;
+                    //Console.WriteLine(Number);
                 }
 
                 if(MultipliedNumber == ReversedNumber)
                 {
-                    LargestPalindromeProduct = MultipliedNumber;
-                    //Console.WriteLine(ReversedNumber);
-                    Console.WriteLine(LargestPalindromeProduct);
+                    Console.WriteLine(MultipliedNumber);
                 }
+
+                NumberTwo = NumberTwo + 1;
             }
+
+            NumberOne = NumberOne + 1;
         }
 
-        //Console.WriteLine(LargestPalindromeProduct);
-        //Console.WriteLine(ReversedNumber);
+        Console.WriteLine(ReversedNumber);
     }
 }
