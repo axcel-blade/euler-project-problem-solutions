e112848ae2605607d43581a420ee03cd58df9f98
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-10-01 18:24:15 +0530

    Update Problem07.cs

--- Problem-07/Problem07.cs
+++ Problem-07/Problem07.cs
@@ -1,9 +1,16 @@
+//10001st Prime
+
 using System;
 
 public class Problem07
 {
     public static void Main(string[] args)
     {
-        
+        int PrimeNumberCount = 1;
+
+        while(PrimeNumberCount != 6)
+        {
+            
+        }
     }
 }
