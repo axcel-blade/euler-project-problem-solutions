39640bfe8af08f120b5bc70f1cb52977f58cc109
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-10-01 16:53:17 +0530

    Update Problem04.cs

--- Problem-04/Problem04.cs
+++ Problem-04/Problem04.cs
@@ -6,12 +6,14 @@ public class Problem04
 {
     public static void Main(string[] args)
     {
-        int ReversedNumber = 0;
+        //int ReversedNumber = 0;
+        int LargestPalindromeProduct = 0;
 
-        for(int NumberOne = 10; NumberOne < 100; NumberOne++)
+        for(int NumberOne = 100; NumberOne < 1000; NumberOne++)
         {
-            for(int NumberTwo = 10; NumberTwo < 100; NumberTwo++)
+            for(int NumberTwo = 100; NumberTwo < 1000; NumberTwo++)
             {
+                int ReversedNumber = 0;
                 int MultipliedNumber = NumberOne * NumberTwo;
                 int Dividend = MultipliedNumber;
                 //Console.WriteLine(Number);
@@ -20,20 +22,24 @@ public class Problem04
                 while(Dividend != 0)
                 {
                     int Remainder = Dividend % 10;
-                    //Console.WriteLine(Remainder);
+                    //Console.WriteLine("Remainder: {0}", Remainder);
 
                     ReversedNumber = (ReversedNumber * 10) + Remainder;
                     //Console.WriteLine(ReversedNumber);
 
                     Dividend/=10;
-                    //Console.WriteLine(Dividend);
+                    //Console.WriteLine("Dividend : {0}", Dividend);
                 }
 
-                //Console.WriteLine(ReversedNumber);
+                //Console.WriteLine("Reversed number : {0}", ReversedNumber);
 
                 if(MultipliedNumber == ReversedNumber)
                 {
-                    Console.WriteLine("Reverse of the {0} is {1}", MultipliedNumber, ReversedNumber);
+                    //Console.WriteLine("{0} x {1} = Reverse of the {2} is {3}", NumberOne, NumberTwo, MultipliedNumber, ReversedNumber);
+                    if(LargestPalindromeProduct < MultipliedNumber)
+                    {
+                        LargestPalindromeProduct = MultipliedNumber;
+                    }
                 }
 
                 //NumberTwo++;
@@ -42,6 +48,6 @@ public class Problem04
             //NumberOne++;
         }
 
-        //Console.WriteLine(ReversedNumber);
+        Console.WriteLine("Largest Palindrome Product : {0}", LargestPalindromeProduct);
     }
 }
