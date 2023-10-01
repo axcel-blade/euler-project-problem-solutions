86fcb69054e5a509b9258f87960f028cf5196d75
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-10-01 17:36:50 +0530

    Update Problem06.cs

--- Problem-06/Problem06.cs
+++ Problem-06/Problem06.cs
@@ -1,9 +1,20 @@
+//Sum Square Difference
+
 using System;
 
 public class Problem06
 {
     public static void Main(string[] args)
     {
-        
+        double SquareSum = 0, Sum = 0;
+
+        for(int i = 1; i < 101; i++)
+        {
+            SquareSum = SquareSum + Math.Pow(i, 2);
+            Sum = Sum + i;
+        }
+
+        double Subtraction = Math.Pow(Sum, 2) - SquareSum;
+        Console.WriteLine(Subtraction);
     }
 }
