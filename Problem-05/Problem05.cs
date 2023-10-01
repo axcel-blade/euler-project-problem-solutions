d657f49df67e1de2953acf1662a31513a3cd2cf2
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-10-01 17:19:46 +0530

    Update Problem05.cs

--- Problem-05/Problem05.cs
+++ Problem-05/Problem05.cs
@@ -1,9 +1,32 @@
+//Smallest Multiple
+
 using System;
 
 public class Problem05
 {
     public static void Main(string[] args)
     {
-        
+        int Number = 1;
+
+        while(Number > 0)
+        {
+            int Count = 0;
+
+            for(int i = 1; i < 21; i++)
+            {
+                if(Number % i == 0)
+                {
+                    Count++;
+
+                    if(Count == 20)
+                    {
+                        Console.WriteLine("Smallest Multiple is {0}", Number);
+                        Number = -2;
+                    }
+                }
+            }
+
+            Number++;
+        }
     }
 }
