06ee86d3fa76e62eeaa34088ec607675d739b472
Srikanth Fernando <56168280+joseph-s-fdo@users.noreply.github.com>
2023-10-03 15:40:27 +0530

    Update Problem07.cs

--- Problem-07/Problem07.cs
+++ Problem-07/Problem07.cs
@@ -6,11 +6,36 @@ public class Problem07
 {
     public static void Main(string[] args)
     {
-        int PrimeNumberCount = 1, Number = 0;
+        int Number = 1, PrimeNumberCount = 0;
 
-        while(PrimeNumberCount != 6)
+        while(PrimeNumberCount < 10002)
         {
-            
+            int Count = 0;
+
+            for(int i = 1; i <= Number; i++)
+            {
+                if(Number % i == 0)
+                {
+                    Count++;
+                    //int Remainder = Number % i;
+                    //Console.WriteLine("{0} % {1} = {2}", Number, i, Remainder);
+                }
+            }
+
+            if(Count == 2)
+            {
+                //Console.WriteLine("Prime number : {0}", Number);
+                PrimeNumberCount++;
+                //Console.WriteLine(PrimeNumberCount);
+
+                if(PrimeNumberCount == 10001)
+                {
+                    Console.WriteLine("10001st prime number : {0}", Number);
+                    break;
+                }
+            }
+
+            Number++;
         }
     }
 }
